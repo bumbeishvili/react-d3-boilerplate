862fd40a89c2281b54078ab336fc98380125a427
bumbeishvili <me@davidb.dev>
2020-10-11 23:11:47 +0400

    Updates

---
+++ precache-manifest.d5df4dd97616ef3bdc3db89303effe8b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8136e8be183e9040a91d900d7d61a6bc",
+    "url": "/react-d3-boilerplate/index.html"
+  },
+  {
+    "revision": "65c0ddfeadba60f3cf43",
+    "url": "/react-d3-boilerplate/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "bedd826c7cc9d1ad9d39",
+    "url": "/react-d3-boilerplate/static/js/2.f62fea64.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-d3-boilerplate/static/js/2.f62fea64.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "65c0ddfeadba60f3cf43",
+    "url": "/react-d3-boilerplate/static/js/main.484b782f.chunk.js"
+  },
+  {
+    "revision": "2a3f556c6b3d2f932a14",
+    "url": "/react-d3-boilerplate/static/js/runtime-main.bd83b347.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-d3-boilerplate/static/media/logo.5d5d9eef.svg"
+  }
+]);
